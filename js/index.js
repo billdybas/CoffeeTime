095dc75c69d59ee4e51d4c45003697b080bf059f
Bill Dybas <wmdybas@gmail.com>
2016-12-13 19:52:38 -0500

    Generate Days in JS, Highlight Current Day, Show Status

--- js/index.js
+++ js/index.js
@@ -1,49 +1,134 @@
+(function init() {
+  var daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+
+  var hours = {
+    'Monday': {
+      'name': 'Monday',
+      'open': '7:30 am',
+      'close': '10:30 pm'
+    },
+    'Tuesday': {
+      'name': 'Tuesday',
+      'open': '7:30 am',
+      'close': '10:30 pm'
+    },
+    'Wednesday': {
+      'name': 'Wednesday',
+      'open': '7:30 am',
+      'close': '10:30 pm'
+    },
+    'Thursday': {
+      'name': 'Thursday',
+      'open': '7:30 am',
+      'close': '10:30 pm'
+    },
+    'Friday': {
+      'name': 'Friday',
+      'open': '7:30 am',
+      'close': '9:00 pm'
+    },
+    'Saturday': {
+      'name': 'Saturday',
+      'open': '11:30 am',
+      'close': '9:00 pm'
+    },
+    'Sunday': {
+      'name': 'Sunday',
+      'open': '11:30 am',
+      'close': '9:00 pm'
+    },
+  };
+
+  function generateDays() {
+    var counter = 1;
+    do {
+      var day = hours[daysOfWeek[counter]];
+      var template = '' +
+        '<tr id="' + day.name + '">' +
+          '<td>' + day.name + '</td>' +
+          '<td>' + day.open + ' -' + (day.name === 'Friday' ? '&nbsp; ' : ' ') + day.close + '</td>' +
+        '</tr>';
+
+      $('#hours').append(template);
+
+      counter = (counter + 1) % 7;
+    } while(counter !== 1);
+  }
+
+  function highlightDay() {
+    $('#' + daysOfWeek[moment().day()]).addClass('highlight');
+  }
+
+  function showStatus() {
+    var todaysHours = hours[daysOfWeek[moment().day()]];
+    var status = $('#status');
+
+    if (moment().isBetween(moment(todaysHours.open, 'HH:mm a'), moment(todaysHours.close, 'HH:mm a'))) {
+      status.html('Open!').removeClass('closed').addClass('open');
+    } else {
+      status.html('Closed!').removeClass('open').addClass('closed');
+    }
+  }
+
+  generateDays();
+  highlightDay();
+  showStatus();
+})();
+
 (function(global){
-    // Image Height & Width
-    var imageHeight = 1080;
-    var imageWidth = 1920;
+  // Image Height & Width
+  var imageHeight = 1080;
+  var imageWidth = 1920;
 
-    // If the Image Doesn't Take Up the Full Frame
-    var innerImageWidth = 1920;
-    var imageLetterBoxWidth = imageWidth - innerImageWidth;
+  // If the Image Doesn't Take Up the Full Frame
+  var innerImageWidth = 1920;
+  var imageLetterBoxWidth = imageWidth - innerImageWidth;
 
-    // Aspect Ratio
-    var imageApspectRatio = imageWidth / imageHeight;
-    var innerImageApspectRatio = innerImageWidth / imageHeight;
+  // Aspect Ratio
+  var imageApspectRatio = imageWidth / imageHeight;
+  var innerImageApspectRatio = innerImageWidth / imageHeight;
 
-    var documentEl = document.documentElement;
+  var documentEl = document.documentElement;
 
-    var image = document.getElementById("image");
+  var image = document.getElementById('image');
 
-    // Resize the Image
-    function resizeImage(){
-        var width, height, scale;
-        var windowWidth = documentEl.clientWidth;
-        var windowHeight = documentEl.clientHeight;
-        var windowAspectRatio = windowWidth / windowHeight;
+  // Resize the Image
+  function resizeImage(){
+    var width, height, scale;
+    var windowWidth = documentEl.clientWidth;
+    var windowHeight = documentEl.clientHeight;
+    var windowAspectRatio = windowWidth / windowHeight;
 
-        // Mobile Phone Screen Shaped
-        if (windowAspectRatio < innerImageApspectRatio) {
-            scale = windowWidth / innerImageWidth;
-            height = windowHeight;
-            width = height * imageApspectRatio;
-        }
-        // Normal Screen Shaped
-        else {
-            scale = windowWidth / innerImageWidth;
-            width = windowWidth + (imageLetterBoxWidth * scale);
-            height = width / imageApspectRatio;
-        }
-
-        image.style.width = width + 'px';
-        image.style.height = height + 'px';
+    // Mobile Phone Screen Shaped
+    if (windowAspectRatio < innerImageApspectRatio) {
+        scale = windowWidth / innerImageWidth;
+        height = windowHeight;
+        width = height * imageApspectRatio;
+    }
+    // Normal Screen Shaped
+    else {
+        scale = windowWidth / innerImageWidth;
+        width = windowWidth + (imageLetterBoxWidth * scale);
+        height = width / imageApspectRatio;
     }
 
-    function onWindowResize() {
-      resizeImage();
-    }
+    image.style.width = width + 'px';
+    image.style.height = height + 'px';
+  }
 
-    global.addEventListener("resize", onWindowResize);
-
+  function onWindowResize() {
     resizeImage();
+  }
+
+  global.addEventListener('resize', onWindowResize);
+
+  resizeImage();
 })(window);
